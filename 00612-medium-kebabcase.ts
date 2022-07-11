60095460d8106b1c4668ef36d8fb3d35c2f1ee13
TakumaIkeda <i.taku0415@gmail.com>
2022-07-11 17:00:51 +0900

    KebabCase

---
+++ 00612-medium-kebabcase.ts
@@ -0,0 +1,26 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<KebabCase<'FooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'fooBarBaz'>, 'foo-bar-baz'>>,
+  Expect<Equal<KebabCase<'foo-bar'>, 'foo-bar'>>,
+  Expect<Equal<KebabCase<'foo_bar'>, 'foo_bar'>>,
+  Expect<Equal<KebabCase<'Foo-Bar'>, 'foo--bar'>>,
+  Expect<Equal<KebabCase<'ABC'>, 'a-b-c'>>,
+  Expect<Equal<KebabCase<'-'>, '-'>>,
+  Expect<Equal<KebabCase<''>, ''>>,
+  Expect<Equal<KebabCase<'😎'>, '😎'>>,
+]
+
+
+// ============= Your Code Here =============
+type UpperCase = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z';
+type KebabCase<S extends string, Result extends string = ''> =
+  S extends `${infer U}${infer V}` ?
+    U extends UpperCase ?
+      Result extends '' ? KebabCase<V, `${Lowercase<U>}`> : KebabCase<V, `${Result}-${Lowercase<U>}`> :
+    KebabCase<V, `${Result}${U}`> :
+  Result
+
+type Test = KebabCase<'FooBarBaz'>
